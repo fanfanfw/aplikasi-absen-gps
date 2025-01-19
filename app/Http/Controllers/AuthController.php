c160e42166b9c4c696ae665caf8d694c8155c16a
fanfanfw <fanfan.firgiawan69@gmail.com>
2025-01-19 21:05:04 +0700

    membuat design halaman presensi gps dan foto selfie

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,15 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
 
 class AuthController extends Controller
 {
     public function proseslogin(Request $request)
     {
         if(Auth::guard('karyawan')->attempt(['nik' => $request->nik, 'password' => $request->password])) {
-            return redirect('/dashboard');            
+        return redirect('/dashboard'); 
         }else{
-            echo "Gagal Login";
+            return redirect('/')->with(['warning' => 'NIK / Password Salah']);
+        }
+    }
+
+    public function proseslogout()
+    {
+        if(Auth::guard("karyawan")->check()){
+           Auth::guard("karyawan")->logout();
+        return redirect('/');
         }
     }
 }
--- app/Models/Karyawan.php
+++ app/Models/Karyawan.php
@@ -12,7 +12,7 @@ class Karyawan extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     protected $table = "karyawan";
-    protected $primarykey = "nik";
+    protected $primaryKey = "nik";
     protected $fillable = [
         'nik',
         'nama_lengkap',
