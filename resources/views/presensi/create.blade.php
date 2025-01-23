f9ca8cc287ee6363893f7003fea742dad175bd4b
fanfanfw <fanfan.firgiawan69@gmail.com>
2025-01-23 14:26:55 +0700

    menampilkan peta lokasi user

--- resources/views/presensi/create.blade.php
+++ resources/views/presensi/create.blade.php
@@ -22,7 +22,16 @@
             border-radius: 15px;
 
         }
+        #map {
+            height: 200px;
+        }
     </style>
+
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"/>
+ <!-- Make sure you put this AFTER Leaflet's CSS -->
+ <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossorigin=""></script>
 @endsection
 @section('content')
 <div class="row" style="margin-top: 70px">
@@ -36,6 +45,11 @@
         <button id="takeabsen" class="btn btn-primary btn-block">
         <ion-icon name="camera-outline"></ion-icon>
             Absen Masuk</button>
+    </div>
+</div>
+<div class="row mt-2">
+    <div class="col">
+        <div id="map"></div>
     </div>
 </div>
 @endsection
@@ -58,6 +72,18 @@
 
     function successCallback(position){
         lokasi.value = position.coords.latitude+","+position.coords.longitude;
+        var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 18);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+        var circle = L.circle([position.coords.latitude, position.coords.longitude], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 20
+        }).addTo(map);
     }
 
     function errorCallback(){
