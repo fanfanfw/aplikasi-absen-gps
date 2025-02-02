c6f0a6d4f20f840a12c4a0a29b182e3fdd6e6071
fanfanfw <fanfan.firgiawan69@gmail.com>
2025-02-02 21:05:09 +0700

    Menghitung Jumlah Jam Keterlambatan

--- resources/views/presensi/monitoring.blade.php
+++ resources/views/presensi/monitoring.blade.php
@@ -25,7 +25,7 @@
                                 <!-- Download SVG icon from http://tabler.io/icons/icon/user -->
                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-calendar"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z" /><path d="M16 3v4" /><path d="M8 3v4" /><path d="M4 11h16" /><path d="M11 15h1" /><path d="M12 15v3" /></svg>
                                     </span>
-                                <input type="text" value="" class="form-control" id="tanggal" name="tanggal" autocomplete="off">
+                                <input type="text" value="{{ date("Y-m-d") }}" class="form-control" id="tanggal" name="tanggal" autocomplete="off">
                             </div>
                             </div>
                         </div>
@@ -67,9 +67,8 @@
         format: 'yyyy-mm-dd' // Format tanggal
     });
 
-    // Event change tanggal
-    $("#tanggal").change(function(e){
-        var tanggal = $(this).val();
+    function loadpresensi(){
+        var tanggal = $("#tanggal").val();
         $.ajax({
             type: 'POST',
             url: '/getpresensi',
@@ -82,6 +81,10 @@
                 $("#loadpresensi").html(response);
             }
         });
+    }
+    // Event change tanggal
+    $("#tanggal").change(function(e){
+        loadpresensi();
     });
 });
     
