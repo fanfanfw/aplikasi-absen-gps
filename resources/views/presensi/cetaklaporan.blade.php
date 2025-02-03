8c9f14686fbff8121873e7943930c1a909d7061a
fanfanfw <fanfan.firgiawan69@gmail.com>
2025-02-03 13:19:29 +0700

    Membuat Laporan Presensi Karyawan

---
+++ resources/views/presensi/cetaklaporan.blade.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>A4</title>
+
+  <!-- Normalize or reset CSS with your favorite library -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
+
+  <!-- Load paper.css for happy printing -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/paper-css/0.4.1/paper.css">
+
+  <!-- Set page size here: A5, A4 or A3 -->
+  <!-- Set also "landscape" if you need -->
+  <style>
+  @page 
+  { 
+    size: A4 
+  }
+  #tittle{
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 18px;
+    font-weight: bold;
+  }
+  .tablekaryawan{
+    margin-top: 50px;
+  }
+  .tablekaryawan, td{
+    padding: 5px;
+  }
+  .tablepresensi{
+    width: 100%;
+    margin-top: 10px;
+    border-collapse: collapse;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  .tablepresensi tr th{
+    border: 2px solid black;
+    padding: 8px;
+    background-color: rgb(190, 190, 190);
+  }
+  .tablepresensi td{
+    border: 2px solid black;
+    padding: 5px;
+    font-size: 14px;
+  }
+  </style>
+</head>
+
+<!-- Set "A5", "A4" or "A3" for class name -->
+<!-- Set also "landscape" if you need -->
+<body class="A4">
+
+  <!-- Each sheet element should have the class "sheet" -->
+  <!-- "padding-**mm" is optional: you can set 10, 15, 20 or 25 -->
+  <section class="sheet padding-10mm">
+
+    <table style="width: 100%;">
+        <tr>
+            <td style="width: 30px">
+                <img src="{{ asset('assets/img/logo-stmik.png') }}" width="150" height="100" alt="">
+            </td>
+            <td>
+                <span id="tittle">
+                    LAPORAN PRESENSI KARYAWAN <br>
+                    PERIODE {{ strtoupper($namabulan[$bulan]) }} - {{ $tahun }}<br>
+                    PT. ROODOLPH DETIK ABADI <br></span>
+                    <span><i>Jl. Raya Hantap No 04, Babakan Surabaya, Kiaracondong, Kota Bandung</i></span>
+            </td>
+        </tr>
+    </table>
+    <table class="tablekaryawan">
+        <tr>
+            <td rowspan="6">
+                @php
+                $path = Storage::url('uploads/karyawan/'. $karyawan->foto);
+                @endphp
+                <img src="{{ url($path) }}" alt="" height="200" width="200">
+            </td>
+        </tr>
+        <tr>
+            <td>NIK</td>
+            <td>:</td>
+            <td>{{ $karyawan->nik }}</td>
+        </tr>
+        <tr>
+            <td>Nama Karyawan</td>
+            <td>:</td>
+            <td>{{ $karyawan->nama_lengkap }}</td>
+        </tr>
+        <tr>
+            <td>Jabatan</td>
+            <td>:</td>
+            <td>{{ $karyawan->jabatan }}</td>
+        </tr>
+        <tr>
+            <td>Departemen</td>
+            <td>:</td>
+            <td>{{ $karyawan->nama_dept }}</td>
+        </tr>
+        <tr>
+            <td>No. Hp</td>
+            <td>:</td>
+            <td>{{ $karyawan->no_hp }}</td>
+        </tr>
+    </table>
+    <table class="tablepresensi">
+        <tr>
+            <th>No.</th>
+            <th>Tanggal</th>
+            <th>Jam Masuk</th>
+            <th>Foto</th>
+            <th>Jam Pulang</th>
+            <th>Foto</th>
+            <th>Keterangan</th>
+        </tr>
+        @foreach ($presensi as $d)
+                @php
+                $path_in = Storage::url('uploads/absensi/'. $d->foto_in);
+                $path_out = Storage::url('uploads/absensi/'. $d->foto_out);
+                @endphp
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ date("d-m-Y", strtotime($d->tgl_presensi)) }}</td>
+                <td>{{ $d->jam_in }}</td>
+                <td><img src="{{ url($path_in) }}" alt="" width="60" height="60"></td>
+                <td>{{ $d->jam_out != null ? $d->jam_out : 'Belum Absen' }}</td>
+                <td><img src="{{ url($path_out) }}" alt="" width="60" height="60"></td>
+                <td>
+                    @if ($d->jam_in > '07:00:00')
+                        Terlambat
+                    @else
+                        Tepat Waktu
+                    @endif
+                </td>
+            </tr>
+        @endforeach
+    </table>
+  </section>
+
+</body>
+
+</html>
